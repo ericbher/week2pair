628b5e516591a186251a5d82599ad719870df3d1
ericbher <ericbher@gmail.com>
2014-10-15 17:18:39 -0500

    code working, needs table styling

--- amortization/interest.php
+++ amortization/interest.php
@@ -1,28 +1,39 @@
 <?php
 
 	
-
+//Grabs the inputs from the form
 $principal = $_POST['loan'];
 $annualInterest = $_POST['interest'];
 $years = $_POST['years'];
 
-$months = $years * 12;
 
+//converts years into months
+$months = $years * 12;
+//converts the annual interest into a decimal point and then divides by 12 for the monthly interest
 $monthInterest = ($annualInterest/100)/12;
 
-$monthRate = $monthInterest * $principal;
+//Rounds the principal amount to six decimal places for precision in calculations
+$beginningAmount = round($principal, 6);
 
-$firstMonth = round(($monthInterest/((pow((1 + $monthInterest), $months)) - 1)) * $principal, 2); /*to principal, 
-first month. this is subtracted from the principal and the result is the remaining balance.*/
-
-$monthPayment = round($firstMonth + $monthRate, 2); // populates first column of table, monthly payment
-
-$toInterest = $monthPayment - $firstMonth; // to interest, this is the interest for the first month.
-
-$balance = $principal - $firstMonth;
+//Creating another variable to store the beginning amount for use in the for loop below
+$a = $beginningAmount;
 
 
 
+$monthRate = round($monthInterest * $beginningAmount, 6);
+
+
+$firstMonth = round(($monthInterest/((pow((1 + $monthInterest), $months)) - 1)) * $beginningAmount, 6); 
+
+
+$monthPayment = round($firstMonth + $monthRate, 6);
+ 
+
+$toInterest = round($monthPayment - $firstMonth, 6); 
+
+
+
+//Creates the table and the column headers
 echo "<table>\n";
 echo "<tr>\n";
 echo "<th scope=\"col\">Month</th>\n";
@@ -31,21 +42,23 @@ echo "<th scope=\"col\">To Principal</th>\n";
 echo "<th scope=\"col\">To Interest</th>\n";
 echo "<th scope=\"col\">Balance</th>\n";
 echo "</tr>\n";
-echo "<tr>\n";
-echo "<td>1</td>\n";
-echo "<td>$monthPayment</td>\n";
-echo "<td>$firstMonth</td>\n";
-echo "<td>$toInterest</td>\n";
-echo "<td>$balance</td>\n";
-echo "</tr>\n"; 
 
 
 
-for ($x = 2; $x <= $months; $x++) {
+
+for ($x = 1; $x <= $months; $x++) {
 	echo "<tr>\n";
 	echo "<td>$x</td>\n";
-	echo "<td>$monthPayment</td>\n"; 
-	
+	$monthRound = round($monthPayment, 2);
+	echo "<td>$monthRound</td>\n"; 
+	$beginningAmount = round($beginningAmount-($monthPayment - ($beginningAmount * $monthInterest)), 6);
+	$precision = number_format($beginningAmount, 2, '.', '');
+	$toInterest = round($a * $monthInterest, 2); //here we're multiplying the beginning Amout by the monthly interest to get the interest payment
+	$toPrincipal = round ($monthRound - $toInterest, 2);
+	echo "<td>$toPrincipal</td>\n";
+	echo "<td>$toInterest</td>\n";
+	echo "<td>$precision</td>\n";
+	$a = $beginningAmount; //here we reset the $a variable to the beginning Amount
 	
 	echo "</tr>\n";
 }
@@ -56,9 +69,14 @@ echo "</table>\n";
 
 
 
-/* At this point you loop through, the remaining balance becomes the $principal - to principle, the
-balance is then multiplied by $monthInterest for the next interest payment, the next interest payment is then subtracted
-from the $monthPayment which results in the principal payment */
+
+
+
+
+
+
+
+
 
 
 
