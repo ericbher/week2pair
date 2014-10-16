909bf2ea28236688cf5967436a28727839919134
Celine <celinesuarez7@gmail.com>
2014-10-16 17:25:28 -0500

    new changes with session added

--- form.php
+++ form.php
@@ -1,10 +1,29 @@
 <?php
+session_start();
+$submit = $_POST['submit'];
+if (isset($submit)){
+
+
+
 $name = $_POST["name"];
+$_SESSION["name"] = $name
+
+
 $email = $_POST["email"];
+$emailvalid = filter_var("$email" , FILTER_VALIDATE_EMAIL);
+if($emailvalid != true){
+	echo "Invalid email.  Please re-enter."
+}
+
+
 $comments = $_POST["comments"];
+$_SESSION["comments"] = $comments
+
+
+
 
 //The support type I'm not too sure about.  I want it to keep its value but I'm not sure where to put the POST
-$supporttype = function(){
+/*$supporttype = function(){
 	if ($supporttype = "billing"){
 		echo "<option value ="billing">;
 	};
@@ -14,7 +33,15 @@ $supporttype = function(){
 	}
 
 	
-};
+};*/
+
+
+
+if(isset($submit)){
+
+}
 echo "<input value= $name>";
 echo "<input value= $email>";
 echo "<input value= $comments>";
+
+}
