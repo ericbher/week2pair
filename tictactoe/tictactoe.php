d6e0f37afe316718212f3c138c600a7373ed0fae
Celine <celinesuarez7@gmail.com>
2014-10-17 16:46:12 -0500

    table made

---
+++ tictactoe/tictactoe.php
@@ -0,0 +1,66 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+ 
+<head>
+    <title>Tic Tac Toe</title>
+ 
+    <style type="text/css">
+    
+    tr 
+    {
+        border: 4px solid black;
+        color:black;
+    }
+
+    td
+    {
+        border:4px;
+        text-align: center;
+        height:75px; 
+        width:75px;
+    }
+ 
+    table
+    {
+        border: 4px solid black;
+        border-collapse:collapse;
+        height:200px;
+        width:200px;
+    }
+ 
+    h2
+    {
+    font-family:Arial; font-size:25px;
+    color:black; font-weight: bold;
+    text-align: center;
+    }
+ 
+</style>
+</head>
+ 
+<body>
+    <h2>Tic Tac Toe</h2>
+    <table>
+        <tr>
+            <td> x </td>
+            <td> x </td>
+            <td> x </td>
+        </tr>
+        <tr>
+            <td> x </td>
+            <td> x </td>
+            <td> x </td>
+        </tr>
+        <tr>
+            <td> x </td>
+            <td> x </td>
+            <td> x </td>
+        </tr>
+    </table>    
+</html>
